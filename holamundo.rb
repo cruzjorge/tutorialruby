9a8a5b3f23688e946a646f4d8322213ec6328dd8
Jorge Cruz <jorge.cruz@sportsworld.com.mx>
2016-03-08 17:06:34 -0600

    Siguiendo curso hasta video 3

---
+++ holamundo.rb
@@ -0,0 +1,16 @@
+=begin
+    Aqui hare las definición de la clase Hola Mundo
+    Colocamos el metodo constructor
+    y por último creamos un metodo saluda
+=end
+class HolaMundo
+    def initialize
+    end
+    def saluda
+        puts "Hola mundo" # Imprimimos el mensaje "Hola Mundo"
+    end
+end
+# Creamos el objeto de la clase Hola Mundo
+objeto = HolaMundo.new()
+objeto.saluda
+gets()
