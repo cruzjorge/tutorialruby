3d866daf45846aa513b7348652e38b7460076648
Jorge Cruz <cruzjorge.mx@gmail.com>
2016-03-22 23:38:59 -0600

    Siguiento tutotial hasta video 15

---
+++ ciclos.rb
@@ -0,0 +1,23 @@
+class HolaMundo
+    def initialize
+    end
+    def saluda
+        puts *(1..10)
+
+        (1..10).each { |i| print "#{i} " }
+        print "\n"
+
+        1.upto(10) { |i| print "#{i} " }
+        print "\n"
+
+        10.downto(1) { |i| print "#{i} " }
+        print "\n"
+
+        10.times { |i| print "#{i} " }
+        print "\n"
+    end
+end
+
+objeto = HolaMundo.new()
+objeto.saluda
+gets()
