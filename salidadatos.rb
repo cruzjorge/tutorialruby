d8596e6e4258b48c02ba9032de1832002e04a5fc
Jorge Cruz <cruzjorge.mx@gmail.com>
2016-03-22 23:51:37 -0600

    Siguiento tutotial hasta video 17

---
+++ salidadatos.rb
@@ -0,0 +1,15 @@
+class HolaMundo
+    def initialize
+    end
+    def saluda
+        print "\t Hola mundo \n"
+        print " y toda la raza\n"
+
+        puts "Hola"
+        puts "mundo"
+    end
+end
+
+objeto = HolaMundo.new()
+objeto.saluda
+gets()
