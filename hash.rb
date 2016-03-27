a777653ac391babadfc5afc472cec53e4e7de816
Jorge Cruz <jorge.cruz@sportsworld.com.mx>
2016-03-26 18:22:53 -0600

    Seguiendo curso hasta video 21

---
+++ hash.rb
@@ -0,0 +1,21 @@
+$ejemplo = "Hola codigo global"
+class HolaMundo
+    def initialize
+    end
+    def saluda
+        cursos = {'ruby'=>21, 'pag_web'=>15, 'html5'=>25}
+        #puts cursos['ruby']
+
+        cursos.each do |key,value|
+            puts "#{key} tiene #{value} videos"
+        end
+
+        indices =  cursos.keys
+        for i in indices
+            puts i
+        end
+    end
+end
+
+objeto = HolaMundo.new()
+objeto.saluda
